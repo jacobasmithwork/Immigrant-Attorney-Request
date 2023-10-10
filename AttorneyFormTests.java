5dc7b7170ac344997b74c3848790166bc7a7b6d4
loftzo <jacobasmithwork@gmail.com>
2023-10-10 15:39:22 -0400

    Updated AF test.

--- AttorneyFormTests.java
+++ AttorneyFormTests.java
@@ -1,11 +1,10 @@
-import static org.junit.Assert.assertTrue;
-
 import org.junit.*;
+import static org.junit.Assert.*;
 
 public class AttorneyFormTests{
     @Test
     public void test1(){
         AttorneyForm af = new AttorneyForm("Bob", "5503 Main Street", "Jim Smith", 123, 42488273);
-        assertTrue("Is true", true);
+        assertEquals("Name Works.","Bob", af.getName());
     }
 }
