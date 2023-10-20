1b2baca4e1333e5d4400fb4411dd18e4ef4a0529
brend <brendanpagano@gmail.com>
2023-10-19 20:25:35 -0400

    Added javadocs to AttorneyForm and Workflow

--- AttorneyForm.java
+++ AttorneyForm.java
@@ -1,15 +1,56 @@
 import java.util.ArrayList;
 
+/**
+ * The Business Object for the Attorney Request functional area.
+ * 
+ */
 public class AttorneyForm{
-    public String name; //Immigrant's Name
-    public String address; //Immigrant's Address
-    public String attorneyName; //Attorney's name
-    public ArrayList<String> comments; //Comments for between steps
-    public long phoneNum; //Attorney's Phone number
-    public int immId; //Immigrant ID
+    /**
+     * The String representing the user's name.
+     */
+    public String name;
+    /**
+     * The String representing the user's address.
+     */
+    public String address;
+    /**
+     * The String representing the name of the user's requested attorney.
+     */
+    public String attorneyName;
+    /**
+     * The ArrayList of Strings for recording notes from the reviewer and approver on the user's attorney request form.
+     */
+    public ArrayList<String> comments;
+    /**
+     * A value of type long representing the requested attorney's phone number.
+     */
+    public long phoneNum;
+    /**
+     * An integer value representing the user's identification number.
+     */
+    public int immId;
+    /**
+     * An integer value representing the status of the user's respective AttorneyForm object.
+     *  A value of 0 represents the form yet to be submitted to review.
+     *  A value of 1 represents the form ready for Review.
+     *  A value of 2 represents the form ready for Approval.
+     */
     public int status; //Form's status through the workflow
-    public int formId; //The trackable unique id of the form, generated as it is inserted into DB / WF.
+    /**
+     * An integer value representing an AttorneyForm object's unique id number that is generated as it is inserted into the 
+     * functional area 3 Database and Workflow object.
+     */
+    public int formId;
     
+    /**The default constructor method for the AttorneyForm.
+     * 
+     * Initializes the form's comment attribute and its status to zero.
+     * @param name is the String value of the user's inputted name. 
+     * @param address is the String value of the user's inputted address.
+     * @param attorneyName is the String value of the name of the user's requested attorney.
+     * @param immId is the interger type id number of user.
+     * @param phoneNum is the long type representation of the user's phone number.
+     */
     public AttorneyForm(String name, String address, String attorneyName, int immId, long phoneNum){
         this.name = name;
         this.address = address;
@@ -20,6 +61,17 @@ public class AttorneyForm{
         this.comments = new ArrayList<String>();
     }
     //Constructor if formId is known (issues with Integer vs int?)
+
+    /**A contructor method for the AttorneyForm that accounts for a known form id number.
+     * 
+     * Initializes the form's comment attribute and its status to zero.
+     * @param name is the String value of the user's inputted name.
+     * @param address is the String value of the user's inputted address.
+     * @param attorneyName is the String value of the name of the user's requested attorney.
+     * @param immId is the interger type id number of user.
+     * @param phoneNum is the long type representation of the user's phone number.
+     * @param formId is the integer value representing an AttorneyForm object's unique id number.
+     */
     public AttorneyForm(String name, String address, String attorneyName, int immId, long phoneNum, Integer formId){
         this.name = name;
         this.address = address;
@@ -31,54 +83,120 @@ public class AttorneyForm{
         this.formId = formId;
     }
     
+    /**A set method for an AttorneyForm object's name attribute.
+     * 
+     * @param name a String representing the user's name.
+     */
     public void setName(String name) {
         this.name = name;
     }
+    /**A set method for an AttorneyForm object's address attribute.
+     * 
+     * @param address a String representing the user's address.
+     */
     public void setAddress(String address) {
         this.address = address;
     }
+    /**A set method for an AttorneyForm object's attorneyName attribute.
+     * 
+     * @param attorneyName a String representing the user's attorney's name.
+     */
     public void setAttorneyName(String attorneyName) {
         this.attorneyName = attorneyName;
     }
+    /**A set method for an AttorneyForm object's immId attribute.
+     * 
+     * @param immId is the interger type id number of user.
+     */
     public void setImmId(int immId) {
         this.immId = immId;
     }
+    /**A set method for an AttorneyForm object's phoneNum attribute.
+     * 
+     * @param phoneNum is the long type representation of the user's phone number.
+     */
     public void setPhoneNum(long phoneNum) {
         this.phoneNum = phoneNum;
     }
+    /**A set method for an AttorneyForm object's status attribute.
+     * 
+     * @param status is the integer value representing an AttorneyForm object's status.
+     */
     public void setStatus(int status) {
         this.status = status;
     }
+    /**A set method for an AttorneyForm object's comments attribute.
+     * 
+     * @param comments is an Arraylist of Strings that represent comments left by the reviewer and approver on the user's form.
+     */
     public void setComments(ArrayList<String> comments) { //More likely to use addComment
         this.comments = comments;
     }
     
+    /**A get method for an AttorneyForm object's name attribute.
+     * 
+     * @return the String of the user's name.
+     */
     public String getName() {
         return name;
     }
+    /**A get method for an AttorneyForm object's address attribute.
+     * 
+     * @return the String of the user's address.
+     */
     public String getAddress() {
         return address;
     }
+    /**A get method for an AttorneyForm object's attorneyName attribute.
+     * 
+     * @return the String of the user's requested attorney's name.
+     */
     public String getAttorneyName() {
         return attorneyName;
     }
+    /**A get method for an AttorneyForm object's immigrant ID attribute.
+     * 
+     * @return an int of the user's identification number.
+     */
     public int getImmId() {
         return immId;
     }
+    /**A get method for an AttorneyForm object's phone number attribute.
+     * 
+     * @return a long of the attorney's phone number.
+     */
     public long getPhoneNum() {
         return phoneNum;
     }
+    /**A get method for an AttorneyForm object's status attribute.
+     * 
+     * @return the int status of the form.
+     */
     public int getStatus() {
         return status;
     }
+    /**A get method for an AttorneyForm object's comments attribute.
+     * 
+     * @return the ArrayList of String comments.
+     */
     public ArrayList<String> getComments() {
         return this.comments;
     }
 
+    /**This method adds comments to an Attorney Form objct's comment attribute.
+     * 
+     * @param comment the String of comments to be appended to the ArrayList of comments.
+     */
     public void addComment(String comment){ //Appends comment to preserve comment history & order
         comments.add(comment);
     }
     
+    /**This method sends an AttorneyForm object to the workflow depending on its status.
+     * A status of 1 will add the object to the readyToReview LinkedList in the Workflow object.
+     * A status of 2 will add the object to the readyToApprove LinkedList in the workflow object.
+     * 
+     * @param status is the integer status to which the object will be updated.
+     */
     public void sendToWf(int status){ //0 = in DE, 1 = send to review queue, 2 = send to approve queue
         this.status = status;
         if(status > 2 || status < 0){
@@ -101,12 +219,19 @@ public class AttorneyForm{
          
     }
 
+    /**This method saves the AttorneyForm object to the database.
+     * 
+     */
     public void sendToDb(){
         //Generate unique form ID based on hash of object
         //Double check via query that ID is not taken, and assign form ID
         //Send to database and save
     }
 
+    
+    /**This method updates an AttorneyForm already contained in the Database.
+     * 
+     */
     public void updateDb(){
         //uses unique form ID to edit the form in database without disrupting others
     }
--- Workflow.java
+++ Workflow.java
@@ -1,25 +1,52 @@
-import java.util.LinkedList;
 
+import java.util.LinkedList;
+/**
+ * The workflow class for the Attorney Request functional area
+ */
 public class Workflow{
+
     //By using Linked Lists as queues, handles easy head popping/peeking and adding.
+    /**
+     * The LinkedList containaing Integer values corresponding to AttorneyForm formId's that are ready for Approval.
+     */
     public static LinkedList<Integer> readyToApprove = new LinkedList<>();
+
+    /**
+     * The LinkedList containaing Integer values corresponding to AttorneyForm formId's that are ready for Approval.
+     */
     public static LinkedList<Integer> readyToReview = new LinkedList<>();
     //Having a static queue shares a single copy of each queue among all instances
 
+    /**This is a get method for the readyToApprove LinkedList.
+     * 
+     * @return A LinkedList of Integer numbers corresponding to AttorneyForm IDs.
+     */
     public static LinkedList<Integer> getApproveQueue(){
         return readyToApprove;
     }
 
+    /**This is a get method for the readyToReview LinkedList.
+     * 
+     * @return A LinkedList of Integer numbers corresponding to AttorneyForm IDs.
+     */
     public static LinkedList<Integer> getReviewQueue(){
         return readyToReview;
     }
 
+    /**This method gets the next AttorneyForm id in the readyToReview LinkedList and removes it from the list.
+     * 
+     * @return A integer value corresponding to the form's ID.
+     */
     public static AttorneyForm getNextReview(){
         int id = readyToReview.remove();
         //return DB.getForm(id)
         return null;
     }
     
+    /**This method gets the next AttorneyForm id in the readyToApprove LinkedList and removes it from the list.
+     * 
+     * @return A integer value corresponding to the form's ID.
+     */
     public static AttorneyForm getNextApproval(){
         int id = readyToReview.remove();
         //return DB.getForm(id)
